4f1a953f022a1d4afa5afcc543564e344214df3b
JD <j.daniel@accenture.com>
2021-07-02 13:49:40 +1000

    extra stuff

--- src/App.js
+++ src/App.js
@@ -33,15 +33,16 @@ const Name = (props) => {
 
   let [fontSize, setFontSize] = useState("11.5vw");
   let [marginTop, setMariginTop] = useState("60vh");
-  let collapse_width = 1100;
 
   useEffect(() => {
+    let collapse_width = 1100;
+
     let josephdaniel = document.getElementById("josephdaniel");
     let elementHeight = josephdaniel.clientHeight;
     let windowHeight = window.innerHeight;
     let marginTopAdjustment = window.innerWidth < collapse_width ? 60 : 30;
     setMariginTop(windowHeight-elementHeight-marginTopAdjustment);
-  })
+  }, [marginTop, setMariginTop])
 
   let style = {
     fontSize, marginTop
@@ -61,38 +62,31 @@ const Header = (props) => {
 
   let headerStyle = {
     float: "right",
-    marginTop: window.outerWidth < collapse_width ? 100 : 0,
-    width: window.outerWidth < collapse_width ? 350 : 800,
+    marginTop: window.innerWidth < collapse_width ? 100 : 0,
+    width: window.innerWidth < collapse_width ? 350 : 800,
   }
 
   let introStyle = {
     float: "left",
-    width: window.outerWidth < collapse_width ? 300 : 400,
+    width: window.innerWidth < collapse_width ? 300 : 400,
     marginLeft: "20px"
   }
 
   let contactStyle = {
     float: "left",
-    width: window.outerWidth < collapse_width ? 300 : 300,
-    marginLeft: window.outerWidth < collapse_width ? 20 : 50,
-    marginTop: window.outerWidth < collapse_width ? 20 : 0,
+    width: window.innerWidth < collapse_width ? 300 : 300,
+    marginLeft: window.innerWidth < collapse_width ? 20 : 50,
+    marginTop: window.innerWidth < collapse_width ? 20 : 0,
   }
 
   let paragraphStyle = {
     height: window.innerWidth < collapse_width ? null : 160
   }
 
-
-
-
-
   return (
     <div style={{ height: "60px" }}>
       <ControlPanel />
-
-
       <div style={headerStyle}>
-
         <div style={introStyle}>
           <Paragraph id="introduction" style={paragraphStyle}>
             G'day, I'm Joseph. I'm a Sydney based <strong> full-stack Software Engineer </strong>. <br /><br />
@@ -100,19 +94,13 @@ const Header = (props) => {
             I sometimes take photos of things I find interesting. <br /><br />
           </Paragraph>
         </div>
-        
         <div style={contactStyle}>
           <Paragraph style={paragraphStyle}>
             <strong> Inquiries </strong>
             <p> joe.daniel@outlook.com.au </p>
           </Paragraph>
-        </div>
-
-        
+        </div>    
       </div>
-
-      
-
     </div>
 
   )
@@ -125,7 +113,25 @@ const App = () => {
       <Header />
       <Name />
       <Banner />
-      {/* <Name /> */}
+      <div>
+        <Paragraph style={{marginTop: 30, float: "right", width: 300}}>
+          <i> What I do </i> <br />
+          <h4> Fontend and Backend Engineering </h4>
+          <h4> Serverless Application Development </h4>
+          <h4> DevOps Engineering </h4>
+          <h4> Application performance Monitoring</h4>
+        </Paragraph>
+      </div>
+      <div>
+        <Paragraph style={{marginTop: 30, float: "right", width: 300}}>
+          <i> Find out more about </i> <br />
+          <h4> The places I've worked </h4>
+          <h4> Some projects I've created </h4>
+          <h4> Some photography </h4>
+        </Paragraph>
+      </div>
+
+      <div style={{height: "100vh"}}></div>
     </div>
   )
   
