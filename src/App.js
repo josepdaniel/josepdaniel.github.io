260875cfa8be7b40b3f7609af70497d946d13228
JD <j.daniel@accenture.com>
2021-07-02 15:27:22 +1000

    More reactive

--- src/App.js
+++ src/App.js
@@ -31,7 +31,6 @@ const ControlPanel = (props) => {
 
 const Name = (props) => {
 
-  let [fontSize, setFontSize] = useState("11.5vw");
   let [marginTop, setMariginTop] = useState("60vh");
 
   useEffect(() => {
@@ -45,62 +44,49 @@ const Name = (props) => {
   }, [marginTop, setMariginTop])
 
   let style = {
-    fontSize, marginTop
+    fontSize: "11.5vw", marginTop
   }
 
   return <h1 id="josephdaniel" style={style}> JOSEPH DANIEL </h1>
 }
 
 const Banner = (props) => {
-  return <img src={banner} width="100%"></img>
+  return <img src={banner} width="100%" alt="Hola"></img>
 }
 
 
 const Header = (props) => {
 
-  let collapse_width = 1100;
-
-  let headerStyle = {
-    float: "right",
-    marginTop: window.innerWidth < collapse_width ? 100 : 0,
-    width: window.innerWidth < collapse_width ? 350 : 800,
+  const ContactInfo = () => {
+    return <div id="contact-info">
+        <Paragraph>
+          <strong> Inquiries </strong>
+          <p> joe.daniel@outlook.com.au </p>
+        </Paragraph>
+      </div>
   }
 
-  let introStyle = {
-    float: "left",
-    width: window.innerWidth < collapse_width ? 300 : 400,
-    marginLeft: "20px"
-  }
-
-  let contactStyle = {
-    float: "left",
-    width: window.innerWidth < collapse_width ? 300 : 300,
-    marginLeft: window.innerWidth < collapse_width ? 20 : 50,
-    marginTop: window.innerWidth < collapse_width ? 20 : 0,
-  }
-
-  let paragraphStyle = {
-    height: window.innerWidth < collapse_width ? null : 160
+  const Introduction = () => {
+    return <div id="gday">
+      <Paragraph>
+        G'day, I'm Joseph. I'm a Sydney based <strong> full-stack Software Engineer </strong>. <br /><br />
+        I like to build cool stuff, learn new things, cook great food, and run long distances (and also, from my problems). <br /><br />
+        I sometimes take photos of things I find interesting. <br /><br />
+      </Paragraph>
+    </div>
   }
 
   return (
     <div style={{ height: "60px" }}>
       <ControlPanel />
-      <div style={headerStyle}>
-        <div style={introStyle}>
-          <Paragraph id="introduction" style={paragraphStyle}>
-            G'day, I'm Joseph. I'm a Sydney based <strong> full-stack Software Engineer </strong>. <br /><br />
-            I like to build cool stuff, learn new things, cook great food, and run long distances (and also, from my problems). <br /><br />
-            I sometimes take photos of things I find interesting. <br /><br />
-          </Paragraph>
-        </div>
-        <div style={contactStyle}>
-          <Paragraph style={paragraphStyle}>
-            <strong> Inquiries </strong>
-            <p> joe.daniel@outlook.com.au </p>
-          </Paragraph>
-        </div>    
+      <div id="welcome">
+        <Introduction />
+        <ContactInfo />
       </div>
+      
+
+      
+
     </div>
 
   )
@@ -113,22 +99,24 @@ const App = () => {
       <Header />
       <Name />
       <Banner />
-      <div>
-        <Paragraph style={{marginTop: 30, float: "right", width: 300}}>
-          <i> What I do </i> <br />
-          <h4> Fontend and Backend Engineering </h4>
-          <h4> Serverless Application Development </h4>
-          <h4> DevOps Engineering </h4>
-          <h4> Application performance Monitoring</h4>
-        </Paragraph>
-      </div>
-      <div>
-        <Paragraph style={{marginTop: 30, float: "right", width: 300}}>
-          <i> Find out more about </i> <br />
-          <h4> The places I've worked </h4>
-          <h4> Some projects I've created </h4>
-          <h4> Some photography </h4>
-        </Paragraph>
+      <div style={{marginRight: 30}}>
+        <div>
+          <Paragraph style={{marginTop: 30, float: "right", width: 300}}>
+            <i> What I do </i> <br />
+            <p> Fontend and Backend Engineering </p>
+            <p> Serverless Application Development </p>
+            <p> DevOps Engineering </p>
+            <p> Application performance Monitoring</p>
+          </Paragraph>
+        </div>
+        <div>
+          <Paragraph style={{marginTop: 30, float: "right", width: 300}}>
+            <i> Find out more about </i> <br />
+            <p> The places I've worked </p>
+            <p> Some projects I've created </p>
+            <p> Some photography </p>
+          </Paragraph>
+        </div>
       </div>
 
       <div style={{height: "100vh"}}></div>
